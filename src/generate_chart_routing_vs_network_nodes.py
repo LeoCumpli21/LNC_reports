b0d3ebee7da968922c9e070aa5adb8380f0df16a
Leonardo Cumplido <cumplileo@gmail.com>
2022-12-02 11:10:45 -0600

    Deleted unnecessary functions

--- src/generate_chart_routing_vs_network_nodes.py
+++ src/generate_chart_routing_vs_network_nodes.py
@@ -1,39 +1,5 @@
-import numpy as np
-import pandas as pd
-import matplotlib as mpl
-import matplotlib.gridspec as gridspec
-import matplotlib.image as mimage
-import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
+from generate_charts import *
 
-
-def get_time(todays_date):
-    """
-    Returns a tuple (date_now, 28_days_ago)
-    representing current date and date 28 days ago
-    """
-
-    # Using current time
-    # t_d = datetime.now()
-    t_d = datetime.strptime(todays_date, '%Y-%m-%d') ### THIS IS JUST FOR TESTING
-    ### AND SHOULD BE CLEARED OUT LATER
-    # 4 weeks ago
-    date_28_days_ago = (t_d - timedelta(days = 28))
-
-    return t_d, date_28_days_ago
-
-def cast_df_date(df):
-    """
-    Casts date column from str to datetime
-
-    df: dataframe of stats with a column named "date"
-
-    returns df
-    """
-
-    df.date = pd.to_datetime(df.date)
-
-    return df
 
 def fix_routing_nodes_stats(df_1, df_2):
     """
@@ -49,7 +15,7 @@ def fix_routing_nodes_stats(df_1, df_2):
 
     return df_1
 
-def plot_net_statistics(f, data1, data2, y1, y2, t_d, date_28_days_ago):
+def plot_routing_vs_net_statistics(f, data1, data2, y1, y2, t_d, date_28_days_ago):
 
     rc = {
             'axes.grid' : True, # for horizontal lines for each y-axis point
