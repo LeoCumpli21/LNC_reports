1ad237ee82f91be2c9ffada0ef5a236029eafc30
Leonardo Cumplido <cumplileo@gmail.com>
2022-12-13 20:57:15 -0600

    changed dates

--- src/generate_chart_routing_vs_network_nodes.py
+++ src/generate_chart_routing_vs_network_nodes.py
@@ -11,16 +11,19 @@ def fix_routing_nodes_stats(df_1, df_2):
     returns df_1 modified
     """
 
-    df_1['routing_nodes'] -= df_2['num_nodes']
+    df_1["routing_nodes"] -= df_2["num_nodes"]
 
     return df_1
 
-def plot_routing_vs_net_statistics(f, data1, data2, y1, y2, t_d, date_28_days_ago):
+
+def plot_routing_vs_net_statistics(
+    f, data1, data2, y1, y2, t_d, date_28_days_ago
+):
 
     rc = {
-            'axes.grid' : True, # for horizontal lines for each y-axis point
-            'grid.color': '#436280',
-            'font.size': 20,
+        "axes.grid": True,  # for horizontal lines for each y-axis point
+        "grid.color": "#436280",
+        "font.size": 20,
     }
     plt.rcParams.update(rc)
 
@@ -37,19 +40,19 @@ def plot_routing_vs_net_statistics(f, data1, data2, y1, y2, t_d, date_28_days_ag
     axes = [ax, ax2]
     for a in axes:
         # Change bottom spine color to be white
-        a.spines['bottom'].set_color('#FFFFFF')
-        a.tick_params(axis='x', colors='#FFFFFF')
-        a.tick_params(axis='y', colors='#FFFFFF')
+        a.spines["bottom"].set_color("#FFFFFF")
+        a.tick_params(axis="x", colors="#FFFFFF")
+        a.tick_params(axis="y", colors="#FFFFFF")
         # Hide the right, left and top spines
-        a.spines['right'].set_visible(False)
-        a.spines['left'].set_visible(False)
-        a.spines['top'].set_visible(False)
+        a.spines["right"].set_visible(False)
+        a.spines["left"].set_visible(False)
+        a.spines["top"].set_visible(False)
     # remove x tick labels from ax 1
-    ax.tick_params(axis='x', labelbottom=False)
+    ax.tick_params(axis="x", labelbottom=False)
 
     # Create labels for data
-    legend_label1 = " ".join([w.capitalize() for w in y1.split('_')])
-    legend_label2 = " ".join([w.capitalize() for w in y2.split('_')])
+    legend_label1 = " ".join([w.capitalize() for w in y1.split("_")])
+    legend_label2 = " ".join([w.capitalize() for w in y2.split("_")])
 
     # choose last 4 weeks
     d1 = data1.loc[date_28_days_ago:]
@@ -58,51 +61,50 @@ def plot_routing_vs_net_statistics(f, data1, data2, y1, y2, t_d, date_28_days_ag
     ######## ROUTING NODES #########
     # Plotting data from 4 weeks ago till 1 week ago
     ax.plot(
-        data1.loc[date_28_days_ago: date_28_days_ago + timedelta(days=21), "date"], # x axis
-        data1.loc[date_28_days_ago: date_28_days_ago + timedelta(days=21), y1], # y axis
+        data1.loc[date_28_days_ago : t_d - timedelta(days=6), "date"],  # x axis
+        data1.loc[date_28_days_ago : t_d - timedelta(days=6), y1],  # y axis
         label=legend_label1,
-        color='#FFFFFF'
+        color="#FFFFFF",
     )
     # y label
-    ax.yaxis.label.set_text('Count')
+    ax.yaxis.label.set_text("Count")
 
     # Plotting this weeks data
     ax.plot(
-        data1.loc[t_d - timedelta(days=7):, "date"], # x axis
-        data1.loc[t_d - timedelta(days=7):, y1], # y axis
-        color='#4EBEB9'
+        data1.loc[t_d - timedelta(days=7) :, "date"],  # x axis
+        data1.loc[t_d - timedelta(days=7) :, y1],  # y axis
+        color="#4EBEB9",
     )
 
     ############### NETWORK NODES #########
     # Plotting data from 4 weeks ago till 1 week ago
     ax2.plot(
         # antes era 22
-        data2.loc[date_28_days_ago: date_28_days_ago + timedelta(days=21), "date"], # x axis
-        data2.loc[date_28_days_ago: date_28_days_ago + timedelta(days=21), y2], # y axis
+        data2.loc[date_28_days_ago : t_d - timedelta(days=6), "date"],  # x axis
+        data2.loc[date_28_days_ago : t_d - timedelta(days=6), y2],  # y axis
         label=legend_label2,
-        color='#FFFFFF'
+        color="#FFFFFF",
     )
     # y label
-    ax2.yaxis.label.set_text('Count')
+    ax2.yaxis.label.set_text("Count")
     # Plotting this weeks data
     ax2.plot(
-        data2.loc[t_d - timedelta(days=7):, "date"], # x axis
-        data2.loc[t_d - timedelta(days=7):, y2], # y axis
-        color='#4EBEB9'
+        data2.loc[t_d - timedelta(days=7) :, "date"],  # x axis
+        data2.loc[t_d - timedelta(days=7) :, y2],  # y axis
+        color="#4EBEB9",
     )
-
 
     # Rotar los ticks del x axis
     ax2.xaxis.set_tick_params(rotation=20)
 
     for a in axes:
         # font size of x & y ticks
-        a.tick_params(axis='both', which='major', labelsize=16)
+        a.tick_params(axis="both", which="major", labelsize=16)
         # change y axis label color
-        a.yaxis.label.set_color('#FFFFFF')
+        a.yaxis.label.set_color("#FFFFFF")
         # axis legend color
-        leg = a.legend(facecolor='#5D89B3', borderpad=0.5)
+        leg = a.legend(facecolor="#5D89B3", borderpad=0.5)
         for text in leg.get_texts():
             text.set_color("#FFFFFF")
-    
+
     return
--- src/generate_chart_total_stats.py
+++ src/generate_chart_total_stats.py
@@ -11,9 +11,9 @@ def plot_net_statistics(f, data, y, t_d, date_28_days_ago):
     """
 
     rc = {
-            'axes.grid' : True, # for horizontal lines for each y-axis point
-            'grid.color': '#436280',
-            'font.size': 20,
+        "axes.grid": True,  # for horizontal lines for each y-axis point
+        "grid.color": "#436280",
+        "font.size": 20,
     }
     plt.rcParams.update(rc)
 
@@ -26,44 +26,43 @@ def plot_net_statistics(f, data, y, t_d, date_28_days_ago):
     ax.set_facecolor(color="#033048")
 
     # Change bottom spine color to be white
-    ax.spines['bottom'].set_color('#FFFFFF')
-    ax.tick_params(axis='x', colors='#FFFFFF')
-    ax.tick_params(axis='y', colors='#FFFFFF')
+    ax.spines["bottom"].set_color("#FFFFFF")
+    ax.tick_params(axis="x", colors="#FFFFFF")
+    ax.tick_params(axis="y", colors="#FFFFFF")
     # Hide the right, left and top spines
-    ax.spines['right'].set_visible(False)
-    ax.spines['left'].set_visible(False)
-    ax.spines['top'].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["left"].set_visible(False)
+    ax.spines["top"].set_visible(False)
 
-    legend_label = " ".join([w.capitalize() for w in y.split('_')])
+    legend_label = " ".join([w.capitalize() for w in y.split("_")])
 
     # choose last 4 weeks data
     data1 = data.loc[date_28_days_ago:]
-    
+
     # Plotting data from 4 weeks ago till 1 week ago
     ax.plot(
-        data1.loc[date_28_days_ago: date_28_days_ago + timedelta(days=21), "date"], # x axis
-        data1.loc[date_28_days_ago: date_28_days_ago + timedelta(days=21), y], # y axis
+        data1.loc[date_28_days_ago : t_d - timedelta(days=6), "date"],  # x axis
+        data1.loc[date_28_days_ago : t_d - timedelta(days=6), y],  # y axis
         label=legend_label,
-        color='#FFFFFF'
+        color="#FFFFFF",
     )
     # Plotting this weeks data
     ax.plot(
-        data1.loc[t_d - timedelta(days=7):, "date"], # x axis
-        data1.loc[t_d - timedelta(days=7):, y], # y axis
-        color='#4EBEB9'
+        data1.loc[t_d - timedelta(days=7) :, "date"],  # x axis
+        data1.loc[t_d - timedelta(days=7) :, y],  # y axis
+        color="#4EBEB9",
     )
 
     # Rotar los ticks del x axis
     ax.xaxis.set_tick_params(rotation=20)
 
     # font size of x & y ticks
-    ax.tick_params(axis='both', which='major', labelsize=16)
+    ax.tick_params(axis="both", which="major", labelsize=16)
     # change y axis label color
-    ax.yaxis.label.set_color('#FFFFFF')
+    ax.yaxis.label.set_color("#FFFFFF")
     # axis legend color
-    leg = ax.legend(facecolor='#5D89B3', borderpad=1)
+    leg = ax.legend(facecolor="#5D89B3", borderpad=1)
     for text in leg.get_texts():
         text.set_color("#FFFFFF")
-    
-    return 
 
+    return
