8925f6a6727dde1212414849fd57a0723537b16c
Leonardo Cumplido <cumplileo@gmail.com>
2022-12-17 12:32:28 -0600

    saving total stats now saves usd btc price

--- src/save_basic_stats.py
+++ src/save_basic_stats.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import json
 import matplotlib.pyplot as plt
+import requests
+import datetime
 from pandas import json_normalize
 import json, gzip, tarfile, io
 from datetime import datetime
@@ -14,32 +16,28 @@ def load_network_basic_stats(filename):
 
     Returns basic stats dataframe
     """
-    network_basic_stats = pd.read_csv(
-        filename,
-        index_col=0
-    )
+    network_basic_stats = pd.read_csv(filename, index_col=0)
     # drop duplicates in case they are
-    network_basic_stats.drop_duplicates(subset='date', keep='first', inplace=True)
+    network_basic_stats.drop_duplicates(
+        subset="date", keep="first", inplace=True
+    )
 
     return network_basic_stats
 
+
 def load_routing_nodes_stats(filename):
 
-    routing_nodes_stats = pd.read_csv(
-        filename,
-        index_col = 0
-    )
+    routing_nodes_stats = pd.read_csv(filename, index_col=0)
 
     return routing_nodes_stats
 
+
 def load_big_nodes_stats(filename):
 
-    big_nodes_stats = pd.read_csv(
-        filename,
-        index_col = 0
-    )
+    big_nodes_stats = pd.read_csv(filename, index_col=0)
 
     return big_nodes_stats
+
 
 def get_total_stats(channels_graph, nodes_graph):
 
@@ -52,6 +50,25 @@ def get_total_stats(channels_graph, nodes_graph):
 
     return network_cap, num_channels, num_nodes
 
+
+def get_btc_price(date: str) -> float:
+
+    timestamp = datetime.datetime.strptime(date, "%Y-%m-%d").timestamp()
+    url = "https://min-api.cryptocompare.com/data/v2/histoday"
+    params = {
+        "fsym": "BTC",
+        "tsym": "USD",
+        "limit": 1,
+        "aggregate": 1,
+        "toTs": timestamp,
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    price = data["Data"]["Data"][0]["close"]
+
+    return price
+
+
 def get_avg_stats(network_cap, num_channels, num_nodes):
     # Average channel size
     avg_chan_size = network_cap / num_channels
@@ -60,19 +77,37 @@ def get_avg_stats(network_cap, num_channels, num_nodes):
 
     return avg_chan_size, avg_node_cap
 
+
 def get_median_stats(channels_graph):
     # Median channel size
-    median_chan_size = channels_graph['capacity'].median() / 100000000
+    median_chan_size = channels_graph["capacity"].median() / 100000000
     return median_chan_size
 
+
 def get_todays_network_data(nodes_graph, channels_graph, todays_date):
-    network_cap, num_channels, num_nodes = get_total_stats(channels_graph, nodes_graph)
-    avg_chan_size, avg_node_cap = get_avg_stats(network_cap, num_channels, num_nodes)
+    network_cap, num_channels, num_nodes = get_total_stats(
+        channels_graph, nodes_graph
+    )
+    avg_chan_size, avg_node_cap = get_avg_stats(
+        network_cap, num_channels, num_nodes
+    )
     med_chan_size = get_median_stats(channels_graph)
+    # dollars capacity
+    dollars_cap = get_btc_price(todays_date) * network_cap
     # todays network data
-    network_data_td = num_nodes, num_channels, network_cap, todays_date, avg_chan_size, avg_node_cap, med_chan_size
+    network_data_td = (
+        num_nodes,
+        num_channels,
+        network_cap,
+        todays_date,
+        avg_chan_size,
+        avg_node_cap,
+        med_chan_size,
+        dollars_cap,
+    )
 
     return network_data_td
+
 
 def add_new_network_stats(df, todays_date):
     """
@@ -90,40 +125,48 @@ def add_new_network_stats(df, todays_date):
     graph = decompress_network_graph(filename)
     nodes_graph, channels_graph = to_pandas_df(graph)
 
-    network_data_td = get_todays_network_data(nodes_graph, channels_graph, todays_date)
+    network_data_td = get_todays_network_data(
+        nodes_graph, channels_graph, todays_date
+    )
     df.loc[len_df] = network_data_td
 
     df.to_csv("data/processed/basic_stats/network_basic_stats.csv")
+
 
 def save_routing_nodes_stats(filename_1, filename_2, todays_date):
     """
     Reads graph and updates routing nodes stats
 
     filename_1: network graph csv file
-    filename_2: routing nodes stats csv file 
+    filename_2: routing nodes stats csv file
     """
 
     ln_graph = pd.read_csv(filename_1, index_col=0)
-    routing_nodes = ln_graph[(ln_graph['total_capacity'] > 1) & (ln_graph['num_channels'] > 10)]
-    rn_stats = (routing_nodes.shape[0], # number of routing nodes
-            routing_nodes['total_capacity'].sum() , # total capacity
-            routing_nodes['num_channels'].sum() , todays_date) # total number of channels
-    
-    last_ix = ln_graph.shape[0] # for placing todays data in the dataframe
+    routing_nodes = ln_graph[
+        (ln_graph["total_capacity"] > 1) & (ln_graph["num_channels"] > 10)
+    ]
+    rn_stats = (
+        routing_nodes.shape[0],  # number of routing nodes
+        routing_nodes["total_capacity"].sum(),  # total capacity
+        routing_nodes["num_channels"].sum(),
+        todays_date,
+    )  # total number of channels
 
-    routing_nodes_stats = pd.read_csv(
-        filename_2,
-        index_col = 0
-    )
+    last_ix = ln_graph.shape[0]  # for placing todays data in the dataframe
+
+    routing_nodes_stats = pd.read_csv(filename_2, index_col=0)
 
     # drop duplicates in case they are
-    routing_nodes_stats.drop_duplicates(subset='date', keep='first', inplace=True)
+    routing_nodes_stats.drop_duplicates(
+        subset="date", keep="first", inplace=True
+    )
     # add today's network stats
     len_df = routing_nodes_stats.shape[0]
     routing_nodes_stats.loc[len_df] = rn_stats
 
     # save new stats to the same csv file
     routing_nodes_stats.to_csv(filename_2)
+
 
 def save_big_nodes_stats(filename_1, filename_2, todays_date):
     """
@@ -135,8 +178,8 @@ def save_big_nodes_stats(filename_1, filename_2, todays_date):
 
     ln_graph = pd.read_csv(filename_1, index_col=0)
     # Industrial size nodes: > 40 BTC
-    big_nodes = ln_graph[(ln_graph['total_capacity'] > 40)]
-    n_chans, t_cap = big_nodes.iloc[:, -2:].sum() 
+    big_nodes = ln_graph[(ln_graph["total_capacity"] > 40)]
+    n_chans, t_cap = big_nodes.iloc[:, -2:].sum()
     # Stats to save
     stats = big_nodes.shape[0], n_chans, t_cap, todays_date
     # Read big nodes stats csv file
@@ -147,6 +190,3 @@ def save_big_nodes_stats(filename_1, filename_2, todays_date):
     big_nodes_stats.loc[sh, :] = stats
     # Save stats
     big_nodes_stats.to_csv(filename_2)
-
-
-
